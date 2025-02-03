e5eefcef4017571c3feed7ddf549f10a33149879
Poojan <poojanjaviya07@gmail.com>
2025-02-03 22:20:27 +0530

    Added input commands in function

--- input.py
+++ input.py
@@ -1,19 +1,26 @@
 # This file is specifically made to take inputs from user (e.g. How many rounds?, number of player,etc.)
 
 import pandas as pd
+def taking_input_storing_in_dataframe():
+    #Taking Input from user
+    total_rounds = int(input("Enter the number of rounds:").strip())
+    player_count = int(input("Enter total number of players playing in the tournaments:").strip())
+    player_names = []
+    player_ratings = []
+    for i in range(player_count):
+        name = input("Enter Player Name:")
+        rating = int(input("Enter the rating of the correspondence player:").strip())
+        player_names.append(name)
+        player_ratings.append(rating)
 
-total_rounds = int(input("Enter Number of Rounds:").strip())
-player_count = int(input("Enter total number of players playing in the tournaments:").strip())
-player_names = []
-player_ratings = []
-
-for i in range(player_count):
-    name = input("Enter Player Name:")
-    rating = int(input("Enter the rating of the correspondence player:").strip())
-    player_names.append(name)
-    player_ratings.append(rating)
-
-df = pd.DataFrame({"Names":player_names,"Rating":player_ratings},index=[i for i in range(player_count)])
-print(df)
-
+    #Storing it in Dataframe
+    df = pd.DataFrame({ "Sr No." : [i for i in range(1,player_count+1)],
+                "Names":player_names,
+                "Rating":player_ratings,
+                    "Points": [0 for i in range(player_count)]},
+                    index=[i for i in range( 1 , player_count+1)])
+    df = df.sort_values(by="Rating", ascending=False)
+    df.index= range( 1 , player_count+1)
+    df.index.name = "Ranking"
+    return df
 
